7870d2b180227e586bcab9bb21ddf7beb91bf9f4
sunzhaoc <suznahc_2011@163.com>
2025-12-12 14:06:56 +0800

    2025-12-12 14:06:56

--- src/components/Plants/PlantGrid.jsx
+++ src/components/Plants/PlantGrid.jsx
@@ -8,12 +8,12 @@ export const plantsData = [
         id: 1,
         name: '大王蚁堡',
         latinName: 'Squamellaria major',
-        price: 2000,
+        price: 5000,
         images: [
-            img1,
-            '/src/assets/images/Squamellaria/Squamellaria major/img.png',
             '/src/assets/images/Squamellaria/Squamellaria major/img_1.png',
             '/src/assets/images/Squamellaria/Squamellaria major/img_2.png',
+            img1,
+            '/src/assets/images/Squamellaria/Squamellaria major/img.png',
         ],
         sizes: ['S', 'M', 'L']
     },
@@ -21,9 +21,10 @@ export const plantsData = [
         id: 2,
         name: '发动机蚁堡',
         latinName: 'Squamellaria kajewskli',
-        price: 2800,
+        price: 2000,
         images: [
             '/src/assets/images/Squamellaria/Squamellaria kajewskli/img.png',
+            '/src/assets/images/Squamellaria/Squamellaria kajewskli/img_1.png',
         ],
         sizes: ['S', 'M', 'L', 'XL']
     },
@@ -31,7 +32,7 @@ export const plantsData = [
         id: 3,
         name: '赫胥黎蚁堡',
         latinName: 'Squamellaria huxleyana',
-        price: 3000,
+        price: 3500,
         images: [
             '/src/assets/images/Squamellaria/Squamellaria huxleyana/img.png',
             '/src/assets/images/Squamellaria/Squamellaria huxleyana/img_1.png',
@@ -42,7 +43,7 @@ export const plantsData = [
         id: 4,
         name: '格雷蚁堡',
         latinName: 'Squamellaria grayi',
-        price: 5800,
+        price: 7000,
         images: [
             '/src/assets/images/Squamellaria/Squamellaria grayi/img.png',
             '/src/assets/images/Squamellaria/Squamellaria grayi/img_1.png',
@@ -53,7 +54,7 @@ export const plantsData = [
         id: 5,
         name: '古皮蚁堡',
         latinName: 'Squamellaria guppyanum',
-        price: 2680,
+        price: 3500,
         images: [
             '/src/assets/images/Squamellaria/Squamellaria guppyanum/img.png',
             '/src/assets/images/Squamellaria/Squamellaria guppyanum/img_1.png',
@@ -114,7 +115,7 @@ export const plantsData = [
         id: 11,
         name: '瓦努阿图蚁堡',
         latinName: 'Squamellaria vanuatuensis',
-        price: 3500,
+        price: 6500,
         images: [
             '/src/assets/images/Squamellaria/Squamellaria vanuatuensis/img.png',
         ],
@@ -124,10 +125,21 @@ export const plantsData = [
         id: 12,
         name: '威尔逊蚁堡',
         latinName: 'Squamellaria wilsonii',
-        price: 4000,
+        price: 5000,
         images: [
             '/src/assets/images/Squamellaria/Squamellaria wilsonii/img.png',
             '/src/assets/images/Squamellaria/Squamellaria wilsonii/img_1.png',
+        ],
+        sizes: ['S', 'M', 'L', 'XL']
+    },
+    {
+        id: 13,
+        name: '威尔金森蚁堡',
+        latinName: 'Squamellaria wilkinsonii',
+        price: 8000,
+        images: [
+            '/src/assets/images/Squamellaria/Squamellaria wilkinsonii/img.png',
+            '/src/assets/images/Squamellaria/Squamellaria wilkinsonii/img_1.png',
         ],
         sizes: ['S', 'M', 'L', 'XL']
     }
