da76fd62c74c1ff7a85efdd0b5c1acc2c78f070c
sunzhaoc <suznahc_2011@163.com>
2025-12-14 16:22:07 +0800

    2025-12-14 16:22:07

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,12 +1,17 @@
+import api from '/src/utils/api';
+
 export const plantApi = {
     getPlantImage: async (imgUrl) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/plant-image?imgUrl=${imgUrl}`);
-            if (!response.ok) throw new Error("获取图片失败");
-            const data = await response.json();
-            console.log(data.url);
+            const response = await api.get('/api/plant-image', {
+                params: {imgUrl},
+            });
+            const data = response.data;
+            if (!data?.url) {
+                throw new Error('返回数据无图片链接');
+            }
+            console.log('图片链接：', data.url);
             return data.url;
-
         } catch (error) {
             console.error('图片请求错误：', error);
             return '/placeholder-image.png'; // 占位图
