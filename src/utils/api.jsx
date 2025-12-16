ec4d81902dc8aedfa0c60d94389d80172b197d8d
sunzhaoc <suznahc_2011@163.com>
2025-12-16 13:48:52 +0800

    2025-12-16 13:48:52

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // TODO 生产环境需要切换！
-    // baseURL: '/api',
-    baseURL: 'http://localhost:8080',
+    baseURL: '/api', // 生产环境
+    // baseURL: 'http://localhost:8080', // 开发环境
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
