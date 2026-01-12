8ee4658dcf5570d38057a11e2db141070c122989
sunzhaoc <suznahc_2011@163.com>
2026-01-12 14:46:15 +0800

    2026-01-12 14:46:15

--- auto_update_git_shell.sh
+++ auto_update_git_shell.sh
@@ -12,6 +12,8 @@ git add .
 git commit -m "$commit_msg"
 git push
 
+echo "打开浏览器输入https://oss.console.aliyun.com/bucket/oss-cn-beijing/public-antplant-store/object/upload上传静态文件"
+
 echo "打开浏览器输入https://cdn.console.aliyun.com/refresh/cache?accounttraceid=9dbe5048f68641fda8317814f941e6e8khad进行刷新预热"
 echo "https://antplant.store/"
 echo "https://antplant.store/index.html"
