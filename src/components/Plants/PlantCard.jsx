d0826adb9c5f2728092783d10f1bc82eb21707a8
sunzhaoc <suznahc_2011@163.com>
2026-02-09 18:15:47 +0800

    2026-02-09 18:15:47

--- src/components/Plants/PlantCard.jsx
+++ src/components/Plants/PlantCard.jsx
@@ -1,12 +1,13 @@
 import {Link} from 'react-router-dom';
 import DBCachedImage from '/src/components/UI/DBCachedImage';
+import styles from '/src/components/Plants/PlantCard.module.css';
 
 export default function PlantCard({plant}) {
     return (<>
         <div className="col-md-3 mb-5">
-            <div className="plant-card-container">
-                <div className="plant-card h-100">
-                    <div className="plant-img-container">
+            <div className={styles.plantCardContainer}>
+                <div className={`${styles.plantCard} h-100`}>
+                    <div className={styles.plantImgContainer}>
                         <Link
                             to={`/detail/${plant.plantId}`}
                             state={{mainPlantInfo: plant}}
@@ -20,25 +21,31 @@ export default function PlantCard({plant}) {
                         </Link>
                     </div>
 
-                    <div className="plant-card-body d-flex flex-column">
+                    <div className={`${styles.plantCardBody} d-flex flex-column`}>
                         {/* 中文名 */}
                         <Link
                             to={`/detail/${plant.plantId}`}
-                            className="plant-name-link"
+                            className={styles.plantNameLink}
                             state={{mainPlantInfo: plant}}
                         >
-                            <h5 className="plant-card-title text-truncate">{plant.plantName}</h5>
+                            <h5 className={`${styles.plantCardTitle} ${styles.textTruncate}`}>
+                                {plant.plantName}
+                            </h5>
                         </Link>
 
                         {/* 拉丁学名 */}
-                        <p className="latin-name-text text-truncate">{plant.plantLatinName}</p>
+                        <p className={`${styles.latinNameText} ${styles.textTruncate}`}>
+                            {plant.plantLatinName}
+                        </p>
 
                         {/* 价格按钮区 */}
-                        <div className="plant-card-footer mt-auto">
-                            <span className="plant-card-footer-purchase-button">¥ {plant.plantMinPrice}</span>
+                        <div className={styles.plantCardFooter}>
+                            <span className={styles.plantCardFooterPurchaseButton}>
+                                ¥ {plant.plantMinPrice}
+                            </span>
                             <Link
                                 to={`/detail/${plant.plantId}`}
-                                className="add-to-cart-btn"
+                                className={styles.addToCartBtn}
                                 state={{mainPlantInfo: plant}}
                             >
                                 购买
