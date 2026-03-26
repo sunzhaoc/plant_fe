2a663f0cc78bf78f67af858c211a4436db1b1a9c
sunzhaoc <suznahc_2011@163.com>
2026-03-26 23:56:40 +0800

    2026-03-26 23:56:40

--- src/utils/debounce.tsx
+++ src/utils/debounce.tsx
@@ -1,12 +1,12 @@
 /**
  * 防抖函数
- * @param {Function} func 要执行的函数
- * @param {Number} delay 延迟时间（ms）
- * @returns {Function} 防抖后的函数
+ * @param func 要执行的函数
+ * @param delay 延迟时间（ms）
+ * @returns 防抖后的函数
  */
-export const debounce = (func, delay = 500) => {
-    let timer = null;
-    return (...args) => {
+export const debounce = (func: (...args: any[]) => void, delay: number = 500) => {
+    let timer: NodeJS.Timeout | number | null = null;
+    return (...args: any[]) => {
         // 清除上一次的定时器
         if (timer) clearTimeout(timer);
         // 重新设置定时器，延迟执行
