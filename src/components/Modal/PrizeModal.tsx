0ea4634e32816d54e621d84bd6ab0f24684fcca1
sunzhaoc <suznahc_2011@163.com>
2026-04-03 16:57:49 +0800

    2026-04-03 16:57:49

--- src/components/Modal/PrizeModal.tsx
+++ src/components/Modal/PrizeModal.tsx
@@ -36,7 +36,7 @@ interface LotteryDrawRes {
     win_code: string;
 }
 
-const WINNING_PROBABILITY = 0.9;
+const WINNING_PROBABILITY = 0.1;
 const LAST_TRIGGER_DATETIME_KEY = 'lastLotteryTriggerDateTime';
 
 /**
