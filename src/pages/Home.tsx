b29f687dfe1fa8bd02f7952bca70d0af48cf5d65
sunzhaoc <suznahc_2011@163.com>
2026-04-06 14:41:08 +0800

    2026-04-06 14:41:08

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,8 +17,8 @@ export default function Home({selectedGenus, selectedIsNew}: HomeProps) {
             <PrizeModal />
 
             <div className="mb-5 text-center">
-                <h1 className="mb-3">✨26年04月订购正式开启（04月05日截单）🎉</h1>
-                <p className="text-muted">🌿探索非凡，分享不寻常的绿意🍃</p>
+                <h1 className="mb-3">🌿探索非凡，分享不寻常的绿意🍃</h1>
+                {/*<p className="text-muted">🌿探索非凡，分享不寻常的绿意🍃</p>*/}
             </div>
 
             <PlantGrid
